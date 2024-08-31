17b0a8bb56cae6b66d86658c31f52f14961b9b06
Emily <em.gonzalezu@duocuc.cl>
2024-08-30 23:33:40 -0400

    "Inter font integration"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   {
     path: 'starter-tab',
     loadChildren: () => import('./pages/starter-tab/starter-tab.module').then( m => m.StarterTabPageModule)
-  },
+  }
 
 ];
 
