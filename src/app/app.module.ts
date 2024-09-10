7d74473b9dcf2e81ad18f3cf35dee2a9088d6d46
Emily Gonzalez <em.gonzalezu@duocuc.cl>
2024-09-09 22:00:04 -0300

    "Scanner integration"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScannerComponent } from './components/scanner/scanner.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ScannerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BarcodeScanner
+  ],
+  
+  
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/scanner/scanner.component.ts
+++ src/app/components/scanner/scanner.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
-  styleUrls: ['./scanner.component.scss'],
+  styleUrls: ['./scanner.component.scss']
 })
-export class ScannerComponent  implements OnInit {
+export class ScannerComponent {
+  scannedCode: string = '';
+  error: string = '';
+  //aCA van los codigos escaneados (para despues compararlos)
+  scannedCodes: string[] = [];
+  scanning: boolean = false;
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
-  constructor() { }
+  //Metodo quelanza el scanner apenas abra la page/tab
+  ionViewDidEnter() {
+    this.startScan(); 
+  }
 
-  ngOnInit() {}
+  startScan() {
+    this.scanning = true;
+    const options: BarcodeScannerOptions = {
+      prompt: 'Escanea el código de barras de tu producto.'
+    };
 
+    this.barcodeScanner.scan(options).then(barcodeData => {
+      this.scannedCode = barcodeData.text;
+      //Almacena el codigo escaneado
+      this.scannedCodes.push(barcodeData.text);
+      this.error = '';
+      this.scanning = false;
+      //Control de errores
+    }).catch(err => {
+      this.error = 'Error: ' + err;
+      this.scannedCode = '';
+      this.scanning = false;
+    });
+  }
+
+  stopScan() {
+    this.scanning = false; // Permite cerrar la vista de escaneo
+  }
+  // Metodo para comparar el codigo escaneado con otros valores "Base de datos"
+  compareScannedCode(codeToCompare: string): boolean {
+    return this.scannedCodes.includes(codeToCompare);
+  }
 }
