ee8bb89d224494cf75b3236ce3f605e614f7b7de
Ignacio <ignacho.acl@gmail.com>
2024-09-28 19:29:49 -0300

    "Google Maps Integration"

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
 
+declare var google: any;
+
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
@@ -11,6 +13,7 @@ export class MapsComponent implements OnInit {
   latitude: number | undefined;
   longitude: number | undefined;
   errorMsg: string | undefined;
+  map: any;
 
   constructor() { }
 
@@ -24,9 +27,26 @@ export class MapsComponent implements OnInit {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
       console.log('Position:', position);
+      this.loadMap();
     } catch (error: any) {
       this.errorMsg = "Error getting location: " + error.message;
     }
+  }
+
+  loadMap() {
+    const mapOptions = {
+      center: { lat: this.latitude, lng: this.longitude },
+      zoom: 17
+    };
+
+    const mapElement = document.getElementById('map');
+    this.map = new google.maps.Map(mapElement, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: { lat: this.latitude, lng: this.longitude },
+      map: this.map
+    });
+
   }
 
 }
