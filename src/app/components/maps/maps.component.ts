b31844bb73a3a316055b6989210aa7e0820a04e3
Ignacio <ignacho.acl@gmail.com>
2024-09-28 18:42:37 -0300

    "Geolocation Integration"

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.scss'],
+  styleUrls: ['./maps.component.scss']
 })
-export class MapsComponent  implements OnInit {
+export class MapsComponent implements OnInit {
+
+  latitude: number | undefined;
+  longitude: number | undefined;
+  errorMsg: string | undefined;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getCurrentLocation();
+  }
+
+  async getCurrentLocation() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      console.log('Position:', position);
+    } catch (error: any) {
+      this.errorMsg = "Error getting location: " + error.message;
+    }
+  }
 
 }
